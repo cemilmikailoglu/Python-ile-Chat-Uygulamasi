abd1585b4c92c7a9cdad873ad8769d172b0bc133
cemil mikayiloğlu <100361416+cemilmikailoglu@users.noreply.github.com>
2023-05-31 10:05:29 +0300

    LastLastV

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import tkinter as tk
 import socket
 import threading
 import time
+
 # * İP ADRESİ MANUEL OLARAK DEĞİŞTİRİLMELİDİR SERVER AÇILDIĞINDA VERİLEN İP ÜZERİNDEN AÇILMALIDIR
 HOST = '192.168.220.89'
 PORT = 8080
 
 def connect(even=None):
+    global username
     global s
     global err1
     
@@ -36,7 +38,7 @@ def connect(even=None):
             time.sleep(0.2)
             eror_label1.config(fg="red")
         return    
-    
+    username = user_name_entry.get()
     eror_label1.pack_forget()
     #*- delete username form----
     user_name_entry.pack_forget()
@@ -51,16 +53,12 @@ def connect(even=None):
     message_frame.pack(padx=10, pady=10)
     message_label.pack(side=tk.LEFT)
     message_entry.pack(side=tk.LEFT)
-    message_entry.bind('<Return>',send_message)
+    message_entry.bind('<Return>', send_message)
     send_button.pack(side=tk.LEFT)
-
-
-
 
 
 def listen():
     while True:
-
         try:
             message = s.recv(1024)
             if message:
@@ -68,19 +66,30 @@ def listen():
                 chat_box.insert(tk.END, message.decode('utf-8'))
                 chat_box.see(tk.END)
                 chat_box.config(state=tk.DISABLED)
-
         except:
             break
 
 def send_message(event=None):
+    global username
     message = message_entry.get()
     if message != "":
-        kullaniciadi = user_name_entry.get()
-        message = "\n" + kullaniciadi + ": " + message
+        # kullaniciadi = user_name_entry.get()
+        message = "\n" + username + ": " + message
         s.send(message.encode('utf-8'))
         message_entry.delete(0, tk.END)
         chat_box.see(tk.END)
 
+def on_close(even=None):
+    global username
+    global a
+    a += 1
+    if a == 13:
+        message = f"{username} sohbetten ayrıldı! güle güle"
+        message = "\n" + message
+        s.send(message.encode('utf-8'))
+        print("closed")
+        s.close()
+        root.quit()
 
 root = tk.Tk()
 root.config(background="orange")
@@ -89,39 +98,31 @@ root.geometry("200x100")
 root.title("Sohbet Uygulaması")
 user_name_frame = tk.Frame(root)
 user_name_frame.pack(padx=20, pady=20)    
-user_name_label = tk.Label(user_name_frame, text="Kullanıcı Adı:",bg='orange')
+user_name_label = tk.Label(user_name_frame, text="Kullanıcı Adı:", bg='orange')
 user_name_label.pack(side=tk.LEFT)
 
 user_name_entry = tk.Entry(user_name_frame, width=15)   
 user_name_entry.pack(side=tk.TOP)
 connect_button = tk.Button(root, text="Bağlan", command=connect)
 connect_button.pack()
-user_name_entry.bind('<Return>',connect)
-
+user_name_entry.bind('<Return>', connect)
 
 chat_frame = tk.Frame(root)
 chat_label = tk.Label(chat_frame, text="Sohbet:")
-chat_box = tk.Text(chat_frame, width=50, height=15,state=tk.DISABLED)
+chat_box = tk.Text(chat_frame, width=50, height=15, state=tk.DISABLED)
 #* ERR
-eror_label1 = tk.Label(text="",bg='orange',font=('Nunito',12),fg="red")
+eror_label1 = tk.Label(text="", bg='orange', font=('Nunito', 12), fg="red")
 err1 = False
 
 message_frame = tk.Frame(root)
+message_label = tk.Label(message_frame, text="Mesaj:")
 
-message_label = tk.Label(message_frame,text="Mesaj:")
-
-message_entry=tk.Entry(message_frame,width=45)
+message_entry = tk.Entry(message_frame, width=45)
 imagem = tk.PhotoImage(file="image-removebg-preview (3).png")
 imagem = imagem.subsample(13, 13)
-send_button=tk.Button(message_frame,image=imagem,command=send_message,border=0,width=40,height=40) 
+send_button = tk.Button(message_frame, image=imagem, command=send_message, border=0, width=40, height=40) 
+a = 0
 
-def on_close():
-    print("closed")
-    s.close()
-    root.quit()
-    
-    
-
-root.protocol('WM_DELTE_WINDOW',on_close)
+root.bind('<Destroy>', on_close)
 
 root.mainloop()
